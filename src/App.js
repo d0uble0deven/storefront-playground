096a70863ff0a76a36893975178c7d72e874c13a
Dev Govindji <dev.govindji.software@gmail.com>
2024-07-14 22:39:18 -0500

    completes SearchBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
-
-// import Card from "./components/Card";
-import MockData from "./MockData.json";
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import MarketPlace from "./pages/MarketPlace";
+
+import MockData from "./MockData.json";
 
 console.log("mockData: ", MockData);
 
--- src/pages/MarketPlace.js
+++ src/pages/MarketPlace.js
@@ -1,15 +1,35 @@
 // src/pages/MarketPlace.js
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../components/Card";
+import SearchBar from "../components/SearchBar";
 
 function MarketPlace({ data }) {
+  const [query, setQuery] = useState("");
+
   console.log(" MarketPlace - data: ", data);
+
+  const handleSearch = (searchQuery) => {
+    setQuery(searchQuery.toLowerCase());
+  };
+
+  const filteredData = data.filter(
+    (item) =>
+      item.title.toLowerCase().includes(query) ||
+      item.category.toLowerCase().includes(query)
+  );
+
   return (
-    <div className="card-list">
-      {data.map((item, index) => (
-        <Card key={index} data={item} />
-      ))}
+    <div>
+      <h1>
+        {query === "" ? `All Models (${data.length})` : `Search for ${query}`}
+      </h1>
+      <SearchBar onSearch={handleSearch} />
+      <div className="card-list">
+        {filteredData.map((item, index) => (
+          <Card key={index} data={item} />
+        ))}
+      </div>
     </div>
   );
 }
